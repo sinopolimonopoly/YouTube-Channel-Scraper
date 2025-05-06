975480e53fb7d7d06d2f78f8c475707ae9b5c7dc
sinopolimonopoly <sinopolicurtis@gmail.com>
2025-05-06 15:27:27 -0400

    Error handling for channels that have no shorts/videos

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from output_scripts.info_outputter import output_info
 from output_scripts.dict_to_csv import create_video_csv
 
 # Channel to scrape
-handle = "maxthemeatguy"
+handle = "AsumSaus"
 # videos (long form), shorts, all_uploads 
-video_type = "shorts"
+video_type = "all_uploads"
 
 # Get channel ID
 channel_id = get_channel_id(handle)
@@ -31,7 +31,13 @@ video_info = get_videos_info(video_ids)
 videos_by_date = dict(sorted(video_info.items(), key = lambda item: item[1]['Numeric Date'], reverse=True))
 
 
-# Output the dictionary to the console (in a table)
-output_info(videos_by_date)
-# Create CSV file for dictionary
-create_video_csv(videos_by_date, handle, video_type)
+# Output to console and create csv file, if request went through
+if bool(video_info) == True:
+    # Output the dictionary to the console (in a table)
+    output_info(videos_by_date)
+
+    # Create CSV file for dictionary, if valid dictionary
+    create_video_csv(videos_by_date, handle, video_type)
+
+else:
+    print("No csv file created. Empty dictionary")
