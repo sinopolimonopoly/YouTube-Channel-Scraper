a03b914c45076a4ce47000f007b2978578db4189
sinopolimonopoly <sinopolicurtis@gmail.com>
2025-05-24 01:37:45 -0400

    Duplicate line removed

--- api_scripts/info/channel_info_getter.py
+++ api_scripts/info/channel_info_getter.py
@@ -23,7 +23,6 @@ def get_channel_info(channel_id):
     channel_info["Handle"] = data['items'][0]['snippet']['customUrl']
     channel_info["Join Date"] = data['items'][0]['snippet']['publishedAt'][0:10]
     channel_info["Description"] = data['items'][0]['snippet']['description']
-    channel_info["Handle"] = data['items'][0]['snippet']['customUrl']
     channel_info["Thumbnail URL"] = data['items'][0]['snippet']['thumbnails']['high']['url'].replace("=s800", "=s176")
 
     channel_info["Sub Count"] = data['items'][0]['statistics']['subscriberCount']
